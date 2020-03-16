977aa6fb5d3acd91a008240962a68da64409c5eb
SantosMaycon <mayconalves.santos14@gmail.com>
2020-03-16 20:55:02 -0300

    Exception handling - findById

--- src/main/java/com/maycon/course/services/UserService.java
+++ src/main/java/com/maycon/course/services/UserService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 
 import com.maycon.course.entities.User;
 import com.maycon.course.repositories.UserRepository;
+import com.maycon.course.resources.exceptions.ResourceExceptionHandler;
+import com.maycon.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +24,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
